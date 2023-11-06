83fe5a531a09e1e623ae4100ccbd8f60e575e201
martina <martinatuverii@gmail.com>
2023-11-06 16:12:25 +0100

    es 4

--- src/pages/pageTuveri.tsx
+++ src/pages/pageTuveri.tsx
@@ -1,8 +1,25 @@
-const PageTuveri = () => {
-    return <div>Martina Tuveri</div>;
-  };
+import Logo from "../components/tuveri/Logo";
+
+function PageTuveri() {
+    const nome = 'Martina';
+    const cognome = 'Tuveri';
+  
+    return (
+      <div>
+        <Logo /> {}
+        <h1>{nome} {cognome}</h1>
+        <p>facendo git pull origin master</p>
+        {}
+      </div>
+    );
+  }
   
   export default PageTuveri;
+  
+  
+  
+  
+  
 
 
 
